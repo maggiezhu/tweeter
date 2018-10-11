09140929c44953d64b01c0269dcdc443d469b580
maggiezhu <tong.zhu@mail.utoronto.ca>
2018-10-11 22:22:31 +0000

    add day1&day2 changes

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function() {
+  // --- our code goes here ---
+
+
+  $('textarea').on('keyup',function(event){
+    var charleft = 140-$(this).val().length;
+    console.log(charleft);
+    $(this).siblings('span').text(charleft);
+    if(charleft<0){
+      $(this).siblings('span').css('color','red');
+    }else{
+      $(this).siblings('span').css('color','black');
+    }
+
+  });
+
+});
+
